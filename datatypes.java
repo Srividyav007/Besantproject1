23be65b6d4594bb5299f21c1c115bc0aa1b3cca5
Srividya007 <srividyav28@gmail.com>
2024-02-10 18:14:53 +0000

    datatype file got added

---
+++ datatypes.java
@@ -0,0 +1,11 @@
+Class demo{
+	public static void main(String[] args){
+		int a=30;
+		int b=40;
+		System.out.println(a+b);
+		String x="hello";
+		string y="system";
+		System.out.println(a+b+x+y);
+	}
+}
+
